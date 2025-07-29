fdb42728d7537de522c74fd5dacbd3b52d57b1e1
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-07-29 09:32:24 +0800

    navbar

---
+++ main/scripts/data.js
@@ -0,0 +1,20 @@
+testimonials = [
+    {
+        text: 'Ang PhilTransApp Tricycle System ay tunay na naging kaibigan ng bawat tricycle driver tulad ko. Hindi lang ito nagbibigay ng tamang direksyon sa ruta, kundi pati na rin ang pag-monitor ng kita at ang mga serbisyo na aming iniaalok.',
+        name: 'Joel A. Pangilinan',
+        role:'Tricycle Driver',
+        stars: '5',
+    },
+    {
+        text: 'Ang PhilTransApp System ay naging game-changer para sa ating tricycle fleet. Ito ay tulad ng pagkakaroon ng isang virtual operations manager na nakasakay! Mula sa pag-optimize ng mga ruta hanggang sa pagsubaybay sa mga iskedyul ng pagpapanatili, pinasimple ng PhilTransApp ang aming pang-araw-araw na operasyon. Lubos na inirerekomenda!',
+        name: 'Jose A. Dela cruz',
+        role:'Operations Manager',
+        stars: '5',
+    },
+    {
+        text: 'Ang PhilTransApp Tricycle System ay talagang nagbago ng paraan kung paano namin pinapatakbo ang aming negosyo ng tricycle. Sa kanyang madaling gamiting interface at kumpletong mga feature, naging mas madali para sa amin ang pammaal sa aming fleet.',
+        name: 'Marzan P. Calma',
+        role:'Tricycle Driver',
+        stars: '5',
+    },
+]
--- src/index.php
+++ src/index.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <title>Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="main.css">
-  <script src="bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="../main.css">
+  <script src="../bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 
 <body class="">
 
 </body>
-    <?php include 'navbar.php'; ?>
+    <?php include 'components/navbar.php'; ?>
 </html>
